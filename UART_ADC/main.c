fac6a88a0634739117679d9db5e38a29575e9007
AnhVu2003 <anv2003bmt@gmail.com>
2024-04-16 21:38:06 +0700

    finished exercise 1 UART_ADC

--- UART_ADC/main.c
+++ UART_ADC/main.c
@@ -1,24 +1,33 @@
 
 #include "stm32f4xx.h"
 #include "system_timetick.h"
+#include "string.h"
 
+#define BUFFER_SIZE 10
+int16_t data;
+char DataBuffer[BUFFER_SIZE];
 
 void init_main(void);
-
+void IntToASCII(int n, char *string);
 int main(void)
 {
 	/* Enable SysTick at 10ms interrupt */
 	SysTick_Config(SystemCoreClock/100);
 
 	init_main();
-
+	data = 12345;
 	while(1){
 		if(tick_count == 100){
-			tick_count = 0;
-			while (USART_GetFlagStatus(UART4, USART_FLAG_TXE) == RESET);
-			USART_SendData(UART4,(uint8_t)65);
-			while (USART_GetFlagStatus(UART4, USART_FLAG_TXE) == RESET);
-			USART_SendData(UART4,(uint8_t)66);
+			tick_count = 0;		
+			IntToASCII(data,DataBuffer);
+			int8_t index = strlen(DataBuffer);
+			while(index >= 0){
+				while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+				USART_SendData(USART2, (uint8_t)DataBuffer[--index]);
+			}
+//			USART_SendData(USART2,(uint8_t)65);		
+//			while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+//			USART_SendData(USART2,(uint8_t)66);
 		}		 
 	}
 }
@@ -31,14 +40,14 @@ void init_main(void)
   /* Enable GPIO clock */
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
   /* Enable UART clock */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
-  /* Connect UART4 pins to AF2 */  
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_UART4);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_UART4); 
+  /* Connect USART2 pins to AF7 */  
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2); 
 
-  /* GPIO Configuration for UART4 Tx */
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
+  /* GPIO Configuration for USART2 Tx */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_2;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
@@ -46,7 +55,7 @@ void init_main(void)
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 
   /* GPIO Configuration for USART Rx */
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_3;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
        
@@ -65,8 +74,20 @@ void init_main(void)
   USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
   USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-  USART_Init(UART4, &USART_InitStructure);
+  USART_Init(USART2, &USART_InitStructure);
 
   /* Enable USART */
-  USART_Cmd(UART4, ENABLE);
+  USART_Cmd(USART2, ENABLE);
+}
+
+void IntToASCII(int n, char *string)
+{
+	uint8_t index = 0;
+	while(n){
+		string[index++] = (n%10+'0');
+		n /= 10;
+	}
+	while(index < BUFFER_SIZE){
+		string[index++] = 0;
+	}
 }
